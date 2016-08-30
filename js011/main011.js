8785acffdcb290e48ad6e8572985bbf796c5852a
Stepan <taivas86@yahoo.com>
2016-08-30 16:40:29 +0300

    Список контактов
    codeacademy (поиск)

---
+++ js011/main011.js
@@ -0,0 +1,31 @@
+var friends = {};
+friends.bill = {
+  firstName: "Proton",
+  lastName: "Professor",
+  number: "+7921784511",
+  address: ['nevsky av','saint-petersburg','Spb','197022']
+};
+friends.steve = {
+  firstName: "Ivan",
+  lastName: "Petrov",
+  number: "+79218852311",
+  address: ['mechnikova str.','Saint-Petesburg','spb','195011']
+};
+
+var list = function(obj) {
+  for(var prop in obj) {
+    alert(prop);
+  }
+};
+
+var search = function(name) {
+  for(var prop in friends) {
+    if(friends[prop].firstName === name) {
+      alert(friends[prop]);
+      return friends[prop];
+    }
+  }
+};
+
+alert (list(friends));
+alert (search ('ivan'));
